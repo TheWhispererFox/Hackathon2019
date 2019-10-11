c988004f64fcaba5a767e11caa657b0e3a90d018
AmiK <amir@AmiK.com>
2019-10-11 23:01:14 +0300

    Added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+    public float movementSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate() => rb.MovePosition(rb.position + Time.fixedDeltaTime * movementSpeed * movement);
+}
