7f8a9325158fa74576ba8f531f67a75c03795ab4
qwaszxerdfcv909 <47119544+qwaszxerdfcv909@users.noreply.github.com>
2019-10-12 11:34:14 +0300

    Menu v0.1

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnClickExit()
+    {
+         
+        Application.Quit();
+
+    }
+
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
