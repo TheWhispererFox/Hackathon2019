f2d1e4648009b4bfba1ff962f22dd8cfd2111b5c
qwaszxerdfcv909 <47119544+qwaszxerdfcv909@users.noreply.github.com>
2019-10-13 11:27:27 +0300

    GamePause

---
+++ Assets/Scripts/Menu_inGame.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Menu_inGame : MonoBehaviour
+{
+    public float timer;
+    public bool ispuse;
+    public bool guipuse;
+
+    void Update()
+    {
+        Time.timeScale = timer;
+        if (Input.GetKeyDown(KeyCode.Escape) && ispuse == false)
+        {
+            ispuse = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && ispuse == true)
+        {
+            ispuse = false;
+        }
+        if (ispuse == true)
+        {
+            timer = 0;
+            guipuse = true;
+
+        }
+        else if (ispuse == false)
+        {
+            timer = 1f;
+            guipuse = false;
+
+        }
+    }
+    public void OnGUI()
+    {
+        if (guipuse == true)
+        {
+            Cursor.visible = true;
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) - 150f, 150f, 45f), "Продолжить"))
+            {
+                ispuse = false;
+                timer = 0;
+                Cursor.visible = false;
+            }
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2)-100f, 150f, 45f), "В Меню"))
+            {
+                ispuse = false;
+                timer = 0;
+                SceneManager.LoadScene(0);
+
+            }
+        }
+    }
+}
